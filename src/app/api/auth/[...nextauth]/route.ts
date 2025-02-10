80b115955c76b663b53c8e7cb93a1eacc6b7af78
Edgar Zea <cesszea@icloud.com>
2025-02-10 15:28:20 -0600

    feat(auth): fix exports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,6 @@ export const authOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
