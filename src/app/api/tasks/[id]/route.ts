5e2c33c228bb33f0fdf9f698997188abee4c5a41
Edgar Zea <cesszea@icloud.com>
2025-02-10 19:24:06 -0600

    fix(vercel): read file

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,10 +1,13 @@
 import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
 import { TaskIT } from '@/types/task';
+import path from 'path';
+
+const folder = path.join(process.cwd(), '/public/tasks.json');
 
 export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
-  const file = await fs.readFile(process.cwd() + '/data/tasks.json', 'utf8');
+  const file = await fs.readFile(folder, 'utf8');
   const { tasks } = JSON.parse(file);
 
   const task = tasks.filter((task: TaskIT) => task.id === id)[0];
@@ -17,7 +20,7 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
 export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
   const {title, details, completed} = await request.json();
-  const file = await fs.readFile(process.cwd() + '/data/tasks.json', 'utf8');
+  const file = await fs.readFile(folder, 'utf8');
   const { tasks } = JSON.parse(file);
 
   const newTasks = tasks.map((task: TaskIT) => {
@@ -30,7 +33,7 @@ export async function PUT(request: Request, { params }: { params: Promise<{ id: 
     return task
   });
 
-  await fs.writeFile(process.cwd() + '/data/tasks.json', JSON.stringify({ tasks: [...newTasks] }));
+  await fs.writeFile(folder, JSON.stringify({ tasks: [...newTasks] }));
 
   return NextResponse.json({
     ok: true,
